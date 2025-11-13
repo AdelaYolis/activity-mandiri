12beb0a85ffb0cb75d1b8f1cb34573a9aa69deb6
AdelaYolis <230712240@students.uajy.ac.id>
2025-11-13 11:58:26 +0700

    all code

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function fn_ValForm(){
+    var sMsg = "";
+    var emailValue = document.getElementById("email").value;
+    var emailPattern = /^[a-z0-9][a-z0-9_\.-]{0,}[a-z0-9]@[a-z0-9][a-z0-9_\.-]{0,}[a-z0-9][\.][a-z0-9]{2,4}$/;
+
+    if(document.getElementById("name").value == ""){
+        sMsg += "\n* Anda belum mengisikan nama";
+    }
+    if(emailValue == ""){
+        sMsg += "\n* Anda belum mengisikan email";
+    }else if(!emailPattern.test(emailValue)){
+        sMsg += "\n* Format Email tidak valid";
+    }
+    if(document.getElementById("message").value == ""){
+        sMsg += "\n* Anda belum mengisikan pesan";
+    }
+
+    if(sMsg != ""){
+        alert("Peringatan:\n" + sMsg);
+        return false;
+    }else{
+        return true;
+    }
+}
